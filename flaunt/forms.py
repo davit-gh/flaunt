069adbd58c46ff4cac8759650e8d1ae058959732
davit-gh <davsmile@yahoo.com>
2014-07-22 16:40:16 +0800

    Styling is almost complete. email conf with amazon ses. Contact form styling functionality.

--- flaunt/forms.py
+++ flaunt/forms.py
@@ -6,7 +6,7 @@ from flaunt.models import Feedback
 import pdb
 class CountryForm(forms.Form):
 	COUNTRY_CHOICES = [('','Please select a country'), ] + [(ctry.country, ctry.country) for ctry in Countrylist.objects.all()]
-	CARRIER_CHOICES = [('','Please select a country first')]
+	CARRIER_CHOICES = [('','Please select shipping type')]
 	CARRIER_TYPES   = [('','Please select a country first')]
 	country = forms.ChoiceField(choices=COUNTRY_CHOICES, widget=forms.Select(attrs={'onchange':'getCountry(this);'}))
 	shipping_type = forms.ChoiceField(choices=CARRIER_TYPES, widget=forms.Select(attrs={'onchange':'setCarriers(this);'}))
