b71958813019009a84a2e483c35b9bdca3bf3d10
davit-gh <davsmile@yahoo.com>
2015-04-06 15:04:32 +0400

    remove men category and change women to product

--- flaunt/context_processors.py
+++ flaunt/context_processors.py
@@ -1,14 +1,14 @@
 from cartridge.shop.models import Category
 
 def get_men_women_categories(request):
-	men_categories = Category.objects.get(title="Men").children.published()
-	women_categories = Category.objects.get(title="Women").children.published()
-	women_titles = map(lambda x: x.title, women_categories)
-	men_titles = map(lambda x: x.title, men_categories)
-	men_subtitles = [[(subcat.title, subcat.slug) for subcat in cat.children.published()] for cat in men_categories]
-	women_subtitles = [[(subcat.title, subcat.slug) for subcat in cat.children.published()] for cat in women_categories]
+	#men_categories = Category.objects.get(title="Men").children.published()
+	product_categories = Category.objects.get(title="Products").children.published()
+	product_titles = map(lambda x: x.title, product_categories)
+	#men_titles = map(lambda x: x.title, men_categories)
+	#men_subtitles = [[(subcat.title, subcat.slug) for subcat in cat.children.published()] for cat in men_categories]
+	product_subtitles = [[(subcat.title, subcat.slug) for subcat in cat.children.published()] for cat in product_categories]
 	context = {
-		'men': dict(zip(men_titles,men_subtitles)),
-		'women': dict(zip(women_titles,women_subtitles)),
+		#'men': dict(zip(men_titles,men_subtitles)),
+		'women': dict(zip(product_titles,product_subtitles)),
 	}
 	return context
