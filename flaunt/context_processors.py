80d977091ea01879707588ce96a7b6312f4f3c81
davit-gh <davsmile@yahoo.com>
2015-04-11 23:02:57 +0400

    fix show/hide shipping detais checkbox

--- flaunt/context_processors.py
+++ flaunt/context_processors.py
@@ -2,7 +2,7 @@ from cartridge.shop.models import Category
 
 def get_men_women_categories(request):
 	#men_categories = Category.objects.get(title="Men").children.published()
-	product_categories = Category.objects.get(title="Women").children.published()
+	product_categories = Category.objects.get(title="Products").children.published()
 	product_titles = map(lambda x: x.title, product_categories)
 	#men_titles = map(lambda x: x.title, men_categories)
 	#men_subtitles = [[(subcat.title, subcat.slug) for subcat in cat.children.published()] for cat in men_categories]
