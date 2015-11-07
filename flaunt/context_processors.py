570d36518c973d6858e29986b08787bfa5171b1b
davit-gh <davsmile@yahoo.com>
2015-11-08 02:30:22 +0400

    divide footer into 4 parts instead of 3, change about us text on home page

--- flaunt/context_processors.py
+++ flaunt/context_processors.py
@@ -1,14 +1,19 @@
 from cartridge.shop.models import Category
 
 def get_men_women_categories(request):
-	#men_categories = Category.objects.get(title="Men").children.published()
-	product_categories = Category.objects.get(title="Products").children.published()
-	product_titles = map(lambda x: x.title, product_categories)
-	#men_titles = map(lambda x: x.title, men_categories)
-	#men_subtitles = [[(subcat.title, subcat.slug) for subcat in cat.children.published()] for cat in men_categories]
-	product_subtitles = [[(subcat.title, subcat.slug) for subcat in cat.children.published()] for cat in product_categories]
+	amd_categories = Category.objects.filter(title__startswith="AMD")
+	intel_categories = Category.objects.filter(title__startswith="intel")
+	#product_categories = Category.objects.get(title="Products").children.published()
+	#product_titles = map(lambda x: x.title, product_categories)
+	amd_titles = map(lambda x: x.title, amd_categories)
+	amd_subtitles = [[(subcat.title, subcat.slug) for subcat in cat.children.published()] for cat in amd_categories]
+
+	intel_titles = map(lambda x: x.title, intel_categories)
+	intel_subtitles = [[(subcat.title, subcat.slug) for subcat in cat.children.published()] for cat in intel_categories]
+	#product_subtitles = [[(subcat.title, subcat.slug) for subcat in cat.children.published()] for cat in product_categories]
 	context = {
-		#'men': dict(zip(men_titles,men_subtitles)),
-		'women': dict(zip(product_titles,product_subtitles)),
+		'amd': dict(zip(amd_titles,amd_subtitles)),
+		'intel': dict(zip(intel_titles,intel_subtitles)),
+		#'women': dict(zip(product_titles,product_subtitles)),
 	}
 	return context
