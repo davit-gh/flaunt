bac8c2b75a412aad7efca76fc1bb71faaf6996a2
davit_am <davsmile@yahoo.com>
2017-08-11 17:11:52 +0400

    change ALLOWED_HOSTS

--- settings.py
+++ settings.py
@@ -225,7 +225,7 @@ MANAGERS = ADMINS
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ['128.199.160.27', 'you-name-it.net', 'www.you-name-it.net']
+ALLOWED_HOSTS = ['128.199.160.27', 'breezg.com', 'www.breezg.com']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
